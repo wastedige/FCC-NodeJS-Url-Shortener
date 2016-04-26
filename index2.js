7288b09a2577bccb722db3abbb9171c71c693283
Shaahin <shaahin@gmail.com>
2016-04-26 00:40:27 -0500

    Minor fixes

--- index2.js
+++ index2.js
@@ -9,7 +9,6 @@ var collection;
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon(__dirname + '/favicon.ico'));
 app.set('port', (process.env.PORT || 5000));
 // views is directory for all template files
 
